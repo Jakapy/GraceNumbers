f19fd37a544c3be93d938a4890a6128a2aa28eff
Jakapy <jaka.porekar.zarki@gmail.com>
2024-04-17 18:01:02 +0200

    Countries,RFact

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { useEffect, useState } from "react";
 import Random from "./Random.jsx";
 import { Input } from "@/components/ui/input";
-
+import { Select } 
+ 
 export default function App() {
   const [fact, setFact] = useState({});
   const [number, setNumber] = useState(0);
+  const [countries, setCountries] = useState([]);
+  const [colors, setColors] = useState([
+    "purple",
+    "black",
+    "white",
+    "blue",
+    "green",
+  ]);
 
   async function getRandomFact(number) {
     const response = await fetch("http://numbersapi.com/random?json");
@@ -18,8 +27,17 @@ export default function App() {
     setFact(data);
   }
 
+  async function getCountries() {
+    const response = await fetch(
+      "https://restcountries.com/v3.1/all?fields=name,flag,region",
+    );
+    const data = await response.json();
+    setCountries(data);
+  }
+
   useEffect(() => {
     getRandomFact();
+    getCountries();
   }, []);
 
   useEffect(() => {
@@ -28,6 +46,16 @@ export default function App() {
 
   return (
     <>
+      <h1>Države</h1>
+      {countries
+        .filter((country) => country.region == "Europe")
+        .map((country) => (
+          <p>{country["name"]["common"]}</p>
+        ))}
+      {colors.map((color) => (
+        <p>{color}</p>
+      ))}
+      <p>_______________________</p>
       <Input
         className="w-32 rounded border-2 border-dotted border-indigo-600 pt-2 text-lg"
         placeholder="Tukaj vpiši številko"
